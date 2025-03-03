026e872266426584923853629f52a7faf0057bad
Hugo Vieira <hugo.vieira2002@hotmail.com>
2025-03-03 08:26:20 +0000

    db: schema complete

--- schemas/db.sql
+++ schemas/db.sql
@@ -1,45 +1,62 @@
-USE teste_hugo;
+USE teste_hugo
 
 CREATE TABLE departments (
-    id INT IDENTITY(1,1) PRIMARY KEY, 
+    id INT IDENTITY(1,1) PRIMARY KEY,
     name NVARCHAR(255) NOT NULL
 );
 
 CREATE TABLE spaces (
     id INT IDENTITY(1,1) PRIMARY KEY,
     name NVARCHAR(255) NOT NULL,
-    departmentId INT NOT NULL,
-    FOREIGN KEY (departmentId) REFERENCES departments(id) ON DELETE CASCADE
+    department INT NOT NULL,
+    FOREIGN KEY (department) REFERENCES departments(id)
+);
+
+CREATE TABLE users (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    username NVARCHAR(255) NOT NULL,
+    email NVARCHAR(255) NOT NULL,
+    password NVARCHAR(255) NOT NULL,
+    created_at DATETIME2(3) NOT NULL DEFAULT SYSDATETIME(),
+    is_admin BIT NOT NULL DEFAULT 0,
+    enabled BIT NOT NULL DEFAULT 1,
+    department INT NOT NULL DEFAULT -1, -- invalid department, choose department on first login
+    FOREIGN KEY (department) REFERENCES departments(id)
 );
 
 CREATE TABLE sheets (
     id INT IDENTITY(1,1) PRIMARY KEY,
-    spaceId INT NOT NULL,
+    space INT NOT NULL,
     createdAt DATETIME NOT NULL DEFAULT GETDATE(),
     signedBy NVARCHAR(255),
     nextDate DATETIME,
-    FOREIGN KEY (spaceId) REFERENCES spaces(id) ON DELETE CASCADE
+    FOREIGN KEY (space) REFERENCES spaces(id)
+);
+
+CREATE TABLE sheet_types (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    name NVARCHAR(255) NOT NULL
 );
 
 CREATE TABLE categories (
     id INT IDENTITY(1,1) PRIMARY KEY,
     name NVARCHAR(255) NOT NULL,
     sheetId INT NOT NULL,
-    FOREIGN KEY (sheetId) REFERENCES sheets(id) ON DELETE CASCADE
+    FOREIGN KEY (sheetId) REFERENCES sheets(id)
 );
 
-CREATE TABLE checklist_items (
+CREATE TABLE checklists (
     id INT IDENTITY(1,1) PRIMARY KEY,
-    categoryId INT NOT NULL,
+    category INT NOT NULL,
     factor NVARCHAR(255) NOT NULL,
     criteria NVARCHAR(MAX) NOT NULL,
     score INT NOT NULL CHECK (score BETWEEN 0 AND 4),
-    FOREIGN KEY (categoryId) REFERENCES categories(id) ON DELETE CASCADE
+    FOREIGN KEY (category) REFERENCES categories(id)
 );
 
 CREATE TABLE noks (
     id INT IDENTITY(1,1) PRIMARY KEY,
-    checklistItemId INT NOT NULL,
+    checklist INT NOT NULL,
     description NVARCHAR(MAX) NOT NULL,
-    FOREIGN KEY (checklistItemId) REFERENCES checklist_items(id) ON DELETE CASCADE
+    FOREIGN KEY (checklist) REFERENCES checklists(id)
 );
