347fd435643565dda8cfbe8d2243af5ae4637c05
Hugo Vieira <hugo.vieira2002@hotmail.com>
2025-03-03 12:02:33 +0000

    feat: listing departments and spaces

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -1,4 +1,3 @@
-import json
 import pyodbc
 from flask import Blueprint, jsonify, render_template
 
@@ -15,9 +14,12 @@ def statistics():
 @bp.route("/dashboard/listings/")
 def index():
     departments = get_departments()
-    print(f"Departments: {departments}")
+    spaces = get_spaces(departments)
     return render_template(
-        "dashboard/listings.html", active_page="listings", departments=departments
+        "dashboard/listings.html",
+        active_page="listings",
+        departments=departments,
+        spaces=spaces,
     )
 
 
@@ -34,6 +36,38 @@ def get_departments():
     except Exception as e:
         print(e)
         return jsonify({"error": f"Ocorreu um erro: {str(e)}"}), 500
+    finally:
+        if "cursor" in locals():
+            cursor.close()
+        if "conn" in locals():
+            conn.close()
+
+
+@bp.route("/api/spaces", methods=["GET"])
+def get_spaces(departments):
+    try:
+        conn = pyodbc.connect(db_conn)
+        cursor = conn.cursor()
+
+        dep_dict = {dep["id"]: dep["name"] for dep in departments}
+
+        # Fetch spaces
+        cursor.execute("SELECT id, name, department FROM spaces")
+        spaces = [
+            {
+                "id": space.id,
+                "name": space.name,
+                "department": dep_dict.get(space.department, "Unknown"),
+            }
+            for space in cursor.fetchall()
+        ]
+
+        return spaces
+
+    except Exception as e:
+        print(e)
+        return jsonify({"error": f"Ocorreu um erro: {str(e)}"}), 500
+
     finally:
         if "cursor" in locals():
             cursor.close()
