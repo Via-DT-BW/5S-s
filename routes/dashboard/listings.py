f3b7c128f69480724b26ca948ecfdf33054baa97
Hugo Vieira <hugo.vieira2002@hotmail.com>
2025-03-05 09:12:24 +0000

    feat: added create forms to add listings data

--- routes/dashboard/listings.py
+++ routes/dashboard/listings.py
@@ -10,11 +10,13 @@ bp = Blueprint("listings", __name__)
 def index():
     departments = get_departments()
     spaces = get_spaces()
+    audit_types = get_audit_types()
     return render_template(
         "dashboard/listings.html",
         active_page="listings",
         departments=departments,
         spaces=spaces,
+        audit_types=audit_types,
     )
 
 
@@ -24,18 +26,29 @@ def get_departments():
     try:
         conn = pyodbc.connect(db_conn)
         cursor = conn.cursor()
-        cursor.execute("""SELECT 
-            d.id, 
-            d.name AS name, 
-            a.name AS audit_type
-        FROM 
-            departments d
-        JOIN 
-            audit_types a ON d.audit_type = a.id
+        cursor.execute("""
+            SELECT 
+                d.id, 
+                d.name AS department_name, 
+                a.name AS audit_type,
+                COUNT(u.id) AS user_count
+            FROM 
+                departments d
+            JOIN 
+                audit_types a ON d.audit_type = a.id
+            LEFT JOIN 
+                users u ON d.id = u.department
+            GROUP BY 
+                d.id, d.name, a.name;
         """)
 
         departments = [
-            {"id": dep.id, "name": dep.name, "audit_type": dep.audit_type}
+            {
+                "id": dep.id,
+                "name": dep.department_name,
+                "audit_type": dep.audit_type,
+                "users_count": dep.user_count,
+            }
             for dep in cursor.fetchall()
         ]
         return departments
@@ -69,11 +82,41 @@ def get_spaces():
             }
             for space in cursor.fetchall()
         ]
+
         return spaces
 
     except Exception as e:
         print(e)
         return jsonify({"error": f"Ocorreu um erro: {str(e)}"}), 500
+
+    finally:
+        if "cursor" in locals() and "conn" in locals():
+            cursor.close()
+            conn.close()
+
+
+@bp.route("/api/audit_types", methods=["GET"])
+def get_audit_types():
+    try:
+        conn = pyodbc.connect(db_conn)
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * FROM audit_types")
+
+        audit_types = [
+            {
+                "id": audit_type.id,
+                "name": audit_type.name,
+            }
+            for audit_type in cursor.fetchall()
+        ]
+
+        return audit_types
+
+    except Exception as e:
+        print(e)
+        return jsonify({"error": f"Ocorreu um erro: {str(e)}"}), 500
+
     finally:
         if "cursor" in locals() and "conn" in locals():
             cursor.close()
