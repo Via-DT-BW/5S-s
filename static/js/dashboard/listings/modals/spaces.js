b94627d1e53ccd626d0916d4e1259dc98b90cb74
Hugo Vieira <hugo.vieira2002@hotmail.com>
2025-03-11 09:55:59 +0000

    feat: edit space frontend

--- static/js/dashboard/listings/modals/spaces.js
+++ static/js/dashboard/listings/modals/spaces.js
@@ -1,4 +1,51 @@
 $(document).ready(function() {
+    $(document).on("click", ".edit-space-btn", function() {
+        let id = $(this).data("id");
+        let name = $(this).data("name");
+        let department = $(this).data("department");
+
+        $('#editSpaceForm #spaceField').val(name);
+
+        let optionsHtml = "";
+        $.each(cachedDepartments, function(_, cachedDepartment) {
+            optionsHtml += `<option value="${cachedDepartment.id}" ${cachedDepartment.name == department ? 'selected' : ''}>${cachedDepartment.name}</option>`;
+        });
+
+        $('#editSpaceForm #departmentField').html(optionsHtml);
+
+        $("#editSpaceForm").data("space-id", id);
+    });
+
+    $(document).on("click", "#editSpaceModalBtn", function(e) {
+        e.preventDefault();
+
+        let id = $("#editSpaceForm").data("space-id")
+        let newName = $("#editSpaceForm #spaceField").val()
+        let newDepartment = $("#editSpaceForm #departmentField").val()
+
+        if (!newName || !newDepartment) {
+            toastr.error("Preencha todos os campos.")
+            return
+        }
+
+        let payload = {
+            name: newName,
+            department: newDepartment,
+        }
+
+        $.ajax({
+            url: `/api/space/${id}`,
+            type: "PUT",
+            contentType: "application/json",
+            data: JSON.stringify(payload),
+            success: function() {
+                toastr.success(`Espaço atualizado com sucesso.`)
+                $("#editSpaceModal").modal("hide")
+                loadSpaces();
+            }
+        })
+    })
+
     $("#create_space_btn").click(function(e) {
         e.preventDefault();
 
@@ -46,5 +93,3 @@ $(document).ready(function() {
         })
     });
 });
-
-
