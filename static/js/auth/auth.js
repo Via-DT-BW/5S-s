5a04e5b6282cce6a678183ae61e3d67c580548fb
Hugo Vieira <hugo.vieira2002@hotmail.com>
2025-03-18 14:46:00 +0000

    wip auth page

--- static/js/auth/auth.js
+++ static/js/auth/auth.js
@@ -1,31 +1,3 @@
-function validateField(field, errorField, errorMsg) {
-    if (!field.val().trim()) {
-        field.addClass("invalidField");
-        errorField.text(errorMsg);
-        return false;
-    }
-
-    field.removeClass("invalidField");
-    errorField.text("");
-
-    return true;
-}
-
-function validatePasswords(pwdField, pwdErrorField, confirmPwdField) {
-    if (pwdField.val() !== confirmPwdField.val()) {
-        pwdField.addClass("invalidField");
-        confirmPwdField.addClass("invalidField");
-        pwdErrorField.text("As passwords não coincidem.");
-        return false;
-    }
-
-    pwdField.removeClass("invalidField");
-    confirmPwdField.removeClass("invalidField");
-    pwdErrorField.text("");
-
-    return true;
-}
-
 $(function() {
     $("#signUp").on("click", function() {
         $("#container").addClass("right-panel-active");
@@ -36,11 +8,11 @@ $(function() {
     });
 
     // Toggle Password Visibility
-    $(".input-group .fa-eye").click(function() {
+    $(".input-group .fa-lock").click(function() {
         let pwdField = $(this).siblings("input");
         let type = pwdField.attr("type") === "password" ? "text" : "password";
         pwdField.attr("type", type);
-        $(this).toggleClass("fa-eye fa-eye-slash");
+        $(this).toggleClass("fa-lock fa-unlock");
     });
 
     // Login Form Submission
@@ -54,8 +26,8 @@ $(function() {
         const pwdErrorField = $("#loginPwdErrorField");
 
         let isValid = true;
-        isValid &= validateField(emailField, emailErrorField, "Introduza o seu e-mail.")
-        isValid &= validateField(pwdField, pwdErrorField, "Introduza a sua palavra-passe.")
+        isValid &= isFieldValid(emailField, emailErrorField, "Introduza o seu e-mail.")
+        isValid &= isFieldValid(pwdField, pwdErrorField, "Introduza a sua palavra-passe.")
 
         if (!isValid) {
             return;
@@ -83,53 +55,108 @@ $(function() {
         })
     });
 
-    // Register Form Submission
     $("#register-form").on("submit", function(e) {
         e.preventDefault();
-
-        const usernameField = $("#registerUsernameField");
-        const usernameErrorField = $("#registerUsernameErrorField");
 
         const emailField = $("#registerEmailField");
         const emailErrorField = $("#registerEmailErrorField");
 
         const pwdField = $("#registerPwdField");
         const pwdErrorField = $("#registerPwdErrorField");
-
         const confirmPwdField = $("#registerConfirmPwdField");
 
         let isValid = true;
-        isValid &= validateField(usernameField, usernameErrorField, "Introduza o nome do seu utilizador.");
-        isValid &= validateField(emailField, emailErrorField, "Introduza o seu e-mail.")
-        isValid &= validateField(pwdField, pwdErrorField, "Introduza a sua palavra-passe.")
-        isValid &= validatePasswords(pwdField, pwdErrorField, confirmPwdField);
+        isValid &= isFieldValid(emailField, emailErrorField, "Introduza o seu e-mail.")
+        isValid &= isFieldValid(pwdField, pwdErrorField, "Introduza a sua palavra-passe.")
+        isValid &= isFieldValid(confirmPwdField, pwdErrorField, "Introduza a sua palavra-passe.")
+        isValid &= arePasswordsEqual(pwdField, pwdErrorField, confirmPwdField);
+        isValid &= isPasswordStrong(pwdField, pwdErrorField);
 
-        if (isValid) {
-            let payload = {
-                username: usernameField.val(),
-                email: emailField.val(),
-                password: pwdField.val(),
-            }
-
-            $.ajax({
-                url: "/api/register",
-                type: "POST",
-                contentType: "application/json",
-                data: JSON.stringify(payload),
-                success: function() {
-                    usernameField.val('');
-                    emailField.val('');
-                    pwdField.val('');
-                    confirmPwdField.val('');
-                    toastr.success("Conta criada com sucesso!");
-                    $("#container").removeClass("right-panel-active");
-                },
-                error: function(xhr) {
-                    let errorMsg = xhr.responseJSON?.error || "Ocorreu um erro ao criar a sua conta.";
-                    toastr.error(errorMsg);
-                }
-            })
+        if (!isValid) {
+            toastr.error("Formulário inválido.")
+            return;
         }
+
+        let payload = {
+            email: emailField.val(),
+            password: pwdField.val(),
+        }
+
+        $.ajax({
+            url: "/api/register",
+            type: "POST",
+            contentType: "application/json",
+            data: JSON.stringify(payload),
+            success: function(responseJSON) {
+                console.log(responseJSON)
+                toastr.success("Conta criada com sucesso!");
+            },
+            error: function(xhr) {
+                let errorMsg = xhr.responseJSON?.error || "Ocorreu um erro ao criar a sua conta.";
+                toastr.error(errorMsg);
+            }
+        })
     });
 });
 
+function isFieldValid(field, errorField, errorMsg) {
+    if (!field.val().trim()) {
+        field.addClass("is-invalid");
+        errorField.text(errorMsg);
+        return false;
+    }
+
+    field.removeClass("is-invalid");
+    errorField.text("");
+
+    return true;
+}
+
+function arePasswordsEqual(pwdField, pwdErrorField, confirmPwdField) {
+    if (pwdField.val() !== confirmPwdField.val()) {
+        pwdField.addClass("is-invalid");
+        confirmPwdField.addClass("is-invalid");
+        pwdErrorField.text("As passwords não coincidem.");
+        return false;
+    }
+
+    pwdField.removeClass("is-invalid");
+    confirmPwdField.removeClass("is-invalid");
+    pwdErrorField.text("");
+
+    return true;
+}
+
+function isPasswordStrong(pwdField, pwdErrorField) {
+    const password = pwdField.val();
+
+    const lengthValid = password.length >= 8;
+    const uppercaseValid = /[A-Z]/.test(password);
+    const specialValid = /[!@#$%^&*()\-_+=\[\]{}|;:'",.<>?/]/.test(password);
+
+    let valid = true;
+
+    pwdErrorField.text("");
+
+    if (!lengthValid || !uppercaseValid || !specialValid) {
+        valid = false;
+    }
+
+    if (!lengthValid) {
+        pwdErrorField.text("A palavra-passe deve ter pelo menos 8 caracteres.");
+    }
+    if (!uppercaseValid) {
+        pwdErrorField.text("A palavra-passe deve conter pelo menos uma letra maiúscula.");
+    }
+    if (!specialValid) {
+        pwdErrorField.text("A palavra-passe deve conter pelo menos dois caracteres especiais.");
+    }
+
+    if (!valid) {
+        pwdField.addClass("is-invalid");
+    } else {
+        pwdField.removeClass("is-invalid");
+    }
+
+    return valid;
+}
