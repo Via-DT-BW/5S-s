01986986f7d5e8bf589cbd3ad8758414bec67457
Hugo Vieira <hugo.vieira2002@hotmail.com>
2025-03-19 09:43:51 +0000

    refactored listings

--- static/js/dashboard/listings/spaces.js
+++ static/js/dashboard/listings/spaces.js
@@ -20,28 +20,16 @@ export function loadSpaces() {
 
         spaces.forEach(space => {
             tbody.append(`
-                    <tr>
-                        <td>${space.name}</td>
-                        <td>${space.department}</td>
-                        <td class="d-flex flex-wrap gap-3">
-                            <button class="btn btn-secondary edit-space-btn" 
-                                data-id="${space.id}" 
-                                data-name="${space.name}" 
-                                data-department="${space.department}" 
-                                data-bs-toggle="modal" 
-                                data-bs-target="#editSpaceModal">
-                                <i class="fa-solid fa-pencil"></i>
-                            </button>
-                            <button class="btn btn-danger delete-space-btn"
-                                data-id="${space.id}" 
-                                data-name="${space.name}" 
-                                data-bs-toggle="modal" 
-                                data-bs-target="#deleteSpaceModal">
-                                <i class="fa-solid fa-trash"></i>
-                            </button>
-                        </td>
-                    </tr>
-                `);
+                <tr class="cursor-pointer"
+                    data-id=${space.id} data-name="${space.name}" data-department=${space.department}
+                    data-bs-toggle="modal" data-bs-target="#editSpaceModal">
+                    <td class="d-flex flex-column">
+                        <b>${space.name}</b>
+                        <small><i>${space.department}</i></small>
+                    </td>
+                    <td><i class="fa-solid fa-angle-right" aria-hidden="true"</i></td>
+                </tr>
+            `);
         });
     }).catch(() => {
         toastr.error("Erro ao carregar os espaços.");
